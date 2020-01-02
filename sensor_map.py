d670b43c20029dc9a3fe8ddbb1dba661cb4904b3
Jaemin Kang <jmkangkr@gmail.com>
2020-01-02 21:46:01 +0900

    w

--- sensor_map.py
+++ sensor_map.py
@@ -1,18 +1,22 @@
 SENSOR_TYPE_DS18B20 = 'DS18B20'
 SENSOR_TYPE_DHT22 = 'DHT22'
 
-SENSOR_MAP = {'boiler-rpi' : [{'PIPE_IN' : (SENSOR_TYPE_DS18B20, '28-020891779157')},
-                              {'PIPE_OUT': (SENSOR_TYPE_DS18B20, '28-0211917790be')},
-                              {'R1': (SENSOR_TYPE_DS18B20, '28-021391770d26')}, 
-                              {'R2': (SENSOR_TYPE_DS18B20, '28-0214917751ce')}],
+SENSOR_MAP = {'boiler-rpi' : [{'OUT_LIVING_ROOM0' : (SENSOR_TYPE_DS18B20, '28-0307977984ce')},
+                              {'OUT_KID_ROOM'     : (SENSOR_TYPE_DS18B20, '28-030897790650')},
+                              {'OUT_BED_ROOM'     : (SENSOR_TYPE_DS18B20, '28-0309977917ea')},
+                              {'OUT_LIVING_ROOM1' : (SENSOR_TYPE_DS18B20, '28-0307977989bd')},
+                              {'OUT_COMPUTER_ROOM': (SENSOR_TYPE_DS18B20, '28-031197798c66')},
+                              {'PIPE_IN'          : (SENSOR_TYPE_DS18B20, '28-031097797ecf')}],
               'bedroom-rpi': [{'BED_ROOM': (SENSOR_TYPE_DHT22, '4')}],
               'kidroom-rpi': [{'KID_ROOM': (SENSOR_TYPE_DHT22, '4')}]
               }
 
 SENSOR_NAMES = (
+    'OUT_LIVING_ROOM0',
+    'OUT_KID_ROOM',
+    'OUT_BED_ROOM',
+    'OUT_LIVING_ROOM1',
+    'OUT_COMPUTER_ROOM',
     'PIPE_IN',
-    'PIPE_OUT',
-    'R1', 
-    'R2',
     'BED_ROOM',
     'KID_ROOM')
